3f16f21bb888e1f87e723f1b4a2dbf3091e2df00
Mariusz Dąbrowski <mariuszd49@gmail.com>
2021-12-30 21:30:29 +0100

    change status

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+@if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div>
+  @endif
 <div class="flex flex-row ">
     <div class="m-20 mr-10 w-full">@include('payment.to-me')</div>
     <div class="m-20 ml-10 w-full" >@include('payment.from-me')</div>
